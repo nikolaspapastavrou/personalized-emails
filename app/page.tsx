d72f07002331d1226cd20e972375b4a27d3933d5
jakebildy <jbildy@uwo.ca>
2023-06-17 13:08:37 -0700

    Set up Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+        <h1 className="text-3xl font-bold text-orange-600">
+          Warm up your email leads! 🔥
+        </h1>
       </div>
 
       <div className={styles.center}>
