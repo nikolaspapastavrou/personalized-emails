809ed6820aa2eae8d5f5a41a9a617cecc4959415
nikolaspapastavrou <103079541+nikolaspapastavrou@users.noreply.github.com>
2023-06-17 12:45:01 -0700

    Update sample.ts

--- api/sample.ts
+++ api/sample.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req, res) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
     res.text('hi').status(200);
   } else {
