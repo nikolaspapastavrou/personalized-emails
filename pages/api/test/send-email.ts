710034c57db6e734402dab234794c7791526b2f1
isaiahb <isaiahballah@gmail.com>
2023-06-17 21:19:28 -0700

    fixed build error

--- pages/api/test/send-email.ts
+++ pages/api/test/send-email.ts
@@ -6,7 +6,7 @@ import * as EmailService from '../../../services/email.service';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
     try {
-      const id = await EmailService.SendEmail("isaiahballah@gmail.com", "Isaiah");
+      const id = await EmailService.SendFirstEmail("isaiahballah@gmail.com", "Isaiah");
       return res.status(200).json({id});
     } catch (error) {
       return res.status(200).json({ error });
