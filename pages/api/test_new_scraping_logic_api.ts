88ab771ed490a3787fde2ad13949453128397ebe
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-06-18 10:30:10 -0700

    fix: Fix email default import

--- pages/api/test_new_scraping_logic_api.ts
+++ pages/api/test_new_scraping_logic_api.ts
@@ -1,5 +1,14 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { scrape_contents_2, get_contents } from '../..//utils/promptUtils'
+import { scrape_contents_2 } from '../..//utils/promptUtils'
+
+import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
+import { PineconeClient } from "@pinecone-database/pinecone";
+import * as dotenv from "dotenv";
+import { Document } from "langchain/document";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   console.log('check');
@@ -9,8 +18,29 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   } else {
     console.log('Starting scraping terrastor!');
     // await scrape_contents_2('https://www.terrastor.co');
-    const contents = await get_contents('https://www.terrastor.co', 'company, products, mission, values');
-    console.log(contents);
+    const namespace = new URL(websiteURL).hostname || '';
+
+    const client = new PineconeClient();
+    await client.init({
+      apiKey: process.env.PINECONE_API_KEY || '',
+      environment: process.env.PINECONE_ENVIRONMENT || '',
+    });
+    const pineconeIndex = client.Index (process.env.PINECONE_INDEX || '');
+  
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings(),
+      { namespace, pineconeIndex },
+    );
+  
+  
+    let pageContents = await vectorStore.similaritySearch('company, products, values', 2);
+  
+    // await delay(5000);
+    // @ts-ignore
+    pageContents = pageContents.map((doc) => {doc.pageContent});
+  
+    console.log(pageContents);
+
     res.status(200).end('hello9!');
   }
 }
