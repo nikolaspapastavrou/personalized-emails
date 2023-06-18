a0561b2ae70eba47865c1df2411b2e8f76dc7565
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-06-18 11:19:36 -0700

    Improve text splitting logic

--- pages/api/test_new_scraping_logic_api.ts
+++ pages/api/test_new_scraping_logic_api.ts
@@ -20,7 +20,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.status(200).end('hi');
   } else {
     console.log('Starting scraping terrastor!');
-    // await scrape_contents_2('https://www.terrastor.co');
+    await scrape_contents_2('https://xcharge.com');
     // const namespace = new URL('https://www.terrastor.co').hostname || '';
 
     // const client = new PineconeClient();
