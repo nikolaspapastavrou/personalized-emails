605e1947715ce93abc7de19a0720e586b818dd15
nikolaspapastavrou <103079541+nikolaspapastavrou@users.noreply.github.com>
2023-06-17 17:11:57 -0700

    Update create_prompt_for_first_email_body.ts

--- pages/api/create_prompt_for_first_email_body.ts
+++ pages/api/create_prompt_for_first_email_body.ts
@@ -3,18 +3,9 @@ import * as cheerio from 'cheerio';
 import { get_contents } from './get_website_contents';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-const handler = async (req: VercelRequest, res: VercelResponse) => {
-
-    console.log('check 1');
-    
-    const { name, company, productDescription, emailTemplate, url } = JSON.parse(req.body);
-
-    console.log('check 2');
+export async function get_subject_line_prompt(name, company, productDescription, emailTemplate, url) {
     const pageContents = await get_contents(url);
 
-    console.log(Object.keys(pageContents));
-
-    console.log('check 3');
     const subjectLinePrompt = `Information scraped from ${company} website:
 ${pageContents}
 
@@ -27,5 +18,19 @@ ${emailTemplate}
 Write a short, captivating, and convincing email to send to ${name} working at ${company} to sell the product described above. Only write the email body text. Do not add a subject line. Do not add quotation marks around the text. Limit the response to 75 words.`;
     res.status(200).end(subjectLinePrompt);
   };
-  
-  export default handler;
+
+export async function get_initial_email_body_prompt(name, company, productDescription, emailTemplate, url) => {
+    const pageContents = await get_contents(url);
+
+    const subjectLinePrompt = `Information scraped from ${company} website:
+${pageContents}
+
+Product to sell:
+${productDescription}
+
+Email Template:
+${emailTemplate}
+
+Write a short, captivating, and convincing email to send to ${name} working at ${company} to sell the product described above. Only write the email body text. Do not add a subject line. Do not add quotation marks around the text. Limit the response to 75 words.`;
+    res.status(200).end(subjectLinePrompt);
+  };
