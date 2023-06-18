edc7351adc42665a7e5c21fe530def30237cf0bc
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-06-17 17:52:38 -0700

    fix: navbar import error

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
-import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
 
 async function getHTML(url: string) {
   try {
@@ -69,21 +68,3 @@ ${emailTemplate}
 Write a short, captivating, and convincing email to send to ${name} working at ${company} to sell the product described above. Only write the email body text. Do not add a subject line. Do not add quotation marks around the text. Limit the response to 75 words.`;
     res.status(200).end(subjectLinePrompt);
   };
-
-export async function getCompletion(prompt) {
-    const payload: OpenAIStreamPayload = {
-            model: "gpt-4",
-            messages: [{ role: "user", content: prompt }],
-            temperature: 0,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-            max_tokens: 1000,
-            stream: true,
-            n: 1,
-      };
-    
-    const stream = await OpenAIStream(payload);
-
-    return stream;
-};
