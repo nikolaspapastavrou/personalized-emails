04908bde06265fb8e7242f82bf5f72f28ddaa5bc
nikolaspapastavrou <nikolaspapastavrou47@gmail.com>
2023-06-18 11:07:14 -0700

    Improve text splitting logic

--- pages/api/get_first_email.ts
+++ pages/api/get_first_email.ts
@@ -53,42 +53,42 @@ export async function getFirstEmail(leadCompanyOperatorName: string, leadCompany
   return { emailSubject: emailSubjectLine.text, emailBody: emailTextBody.text };
 };
 
-export async function getReply(sourceProductDescription: string, leadCompanyUrl: string, emailConversation: string[] ): Promise<string> {
-  console.log(sourceProductDescription);
-  console.log(leadCompanyUrl);
-  console.log(emailConversation);
+// export async function getReply(sourceProductDescription: string, leadCompanyUrl: string, emailConversation: string[] ): Promise<string> {
+//   console.log(sourceProductDescription);
+//   console.log(leadCompanyUrl);
+//   console.log(emailConversation);
 
-  // Get company info from url
-  console.log('Getting company info');
-  const companyInfo = await promptUtils.get_contents(leadCompanyUrl || '', 'Company information, products, solutions, values, mission');
+//   // Get company info from url
+//   console.log('Getting company info');
+//   const companyInfo = await promptUtils.get_contents(leadCompanyUrl || '', 'Company information, products, solutions, values, mission');
 
-  // Create prompts
-  console.log('Constructing prompts');
-  const subjectLinePrompt = await promptUtils.get_subject_line_prompt(leadCompanyOperatorName, leadCompanyName, sourceProductDescription, companyInfo, sourceEmailTemplate);
-  console.log('Got subject line');
-  console.log(subjectLinePrompt);
-  const emailBodyPrompt = await promptUtils.get_email_body_prompt(leadCompanyOperatorName, leadCompanyName, sourceProductDescription, companyInfo, sourceEmailTemplate);
-  console.log('Got email body');
-  console.log(emailBodyPrompt);
+//   // Create prompts
+//   console.log('Constructing prompts');
+//   const subjectLinePrompt = await promptUtils.get_subject_line_prompt(leadCompanyOperatorName, leadCompanyName, sourceProductDescription, companyInfo, sourceEmailTemplate);
+//   console.log('Got subject line');
+//   console.log(subjectLinePrompt);
+//   const emailBodyPrompt = await promptUtils.get_email_body_prompt(leadCompanyOperatorName, leadCompanyName, sourceProductDescription, companyInfo, sourceEmailTemplate);
+//   console.log('Got email body');
+//   console.log(emailBodyPrompt);
 
-  // Create Langchain chains from prompts
-  console.log('Creating langchain chains from prompts');
-  const chat = new ChatOpenAI({ temperature: 0 });
+//   // Create Langchain chains from prompts
+//   console.log('Creating langchain chains from prompts');
+//   const chat = new ChatOpenAI({ temperature: 0 });
 
-  // get completions from Langchain chains
-  console.log('Creating langchain chains from prompts');
-  const emailSubjectLine = await chat.call([
-    new HumanChatMessage(
-      subjectLinePrompt,
-    ),
-  ]);
-  const emailTextBody = await chat.call([
-    new HumanChatMessage(
-      emailBodyPrompt
-    ),
-  ]);
+//   // get completions from Langchain chains
+//   console.log('Creating langchain chains from prompts');
+//   const emailSubjectLine = await chat.call([
+//     new HumanChatMessage(
+//       subjectLinePrompt,
+//     ),
+//   ]);
+//   const emailTextBody = await chat.call([
+//     new HumanChatMessage(
+//       emailBodyPrompt
+//     ),
+//   ]);
 
-  // Return response
-  console.log('Returning response');
-  return { emailSubject: emailSubjectLine.text, emailBody: emailTextBody.text };
-};
+//   // Return response
+//   console.log('Returning response');
+//   return { emailSubject: emailSubjectLine.text, emailBody: emailTextBody.text };
+// };
